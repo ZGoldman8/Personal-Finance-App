67fc9a3633b35103f9db3cc362dc9b9e9c47f81e
Zach Goldman <zgoldman@protonmail.com>
2021-07-22 02:11:37 -0700

    Add NetWorthActivity
    Created layout, created activity, functionality still needs work

--- app/src/main/java/com/example/personalfinanceapp/Frag1.java
+++ app/src/main/java/com/example/personalfinanceapp/Frag1.java
@@ -1,18 +1,35 @@
 package com.example.personalfinanceapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Frag1 extends Fragment {
+    Button netWorth;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       return inflater.inflate(R.layout.frag1_layout, container, false);
+        View view = inflater.inflate(R.layout.frag1_layout, container, false);
+        netWorth = view.findViewById(R.id.netWorth);
+
+        netWorth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNetWorth();
+            }
+        });
+        return view;
+    }
+
+    public void openNetWorth() {
+        Intent intent = new Intent(Frag1.this.getActivity(), NetWorthActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/personalfinanceapp/NetWorthActivity.java
@@ -0,0 +1,9 @@
+package com.example.personalfinanceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NetWorthActivity extends AppCompatActivity {
+     public NetWorthActivity() {
+        setContentView(R.layout.frag1_networth_layout);
+    }
+}
