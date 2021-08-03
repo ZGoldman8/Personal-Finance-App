c8cf962ff79e4c4a95955709eb8a6bf8e547e693
Zach Goldman <zgoldman@protonmail.com>
2021-08-03 07:35:07 -0700

    Dynamic Add and Remove from Asset Inventory
    Add and remove buttons will dynamically populate the screen, still some
    formatting issues but the functionality works

--- app/src/main/java/com/example/personalfinanceapp/AssetsInventoryActivity.java
+++ app/src/main/java/com/example/personalfinanceapp/AssetsInventoryActivity.java
@@ -3,17 +3,15 @@ package com.example.personalfinanceapp;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AssetsInventoryActivity extends AppCompatActivity {
-    int numberOfAssetsInList = 1;
+    int numberOfFieldsInList = 1;
     FloatingActionButton addAssetsInventoryItem;
     FloatingActionButton removeAssetsInventoryItem;
 
@@ -22,22 +20,30 @@ public class AssetsInventoryActivity extends AppCompatActivity {
         setContentView(R.layout.frag1_assets_inventory_layout);
         addAssetsInventoryItem = (FloatingActionButton) findViewById(R.id.addAssetsInventoryItemButton);
         removeAssetsInventoryItem = (FloatingActionButton) findViewById(R.id.removeAssetsInventoryItemButton);
-        removeAssetsInventoryItem.setVisibility(View.GONE);
+        removeAssetsInventoryItem.setVisibility(View.INVISIBLE);
 
         addAssetsInventoryItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 addAssetsInventoryItem();
-                showRemoveButton();
+                showOrHideRemoveButton();
+            }
+        });
+
+        removeAssetsInventoryItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeAssetsInventoryItem();
+                showOrHideRemoveButton();
             }
         });
     };
 
-    public void showRemoveButton() {
-        if (numberOfAssetsInList > 1) {
+    public void showOrHideRemoveButton() {
+        if (numberOfFieldsInList > 1) {
             removeAssetsInventoryItem.setVisibility(View.VISIBLE);
         } else {
-            removeAssetsInventoryItem.setVisibility(View.GONE);
+            removeAssetsInventoryItem.setVisibility(View.INVISIBLE);
         }
     }
 
@@ -48,24 +54,22 @@ public class AssetsInventoryActivity extends AppCompatActivity {
         EditText et2 = new EditText(this);
         et1.setLayoutParams(p);
         et2.setLayoutParams(p);
-        et1.setHint("Asset Name");
-        et2.setHint("Asset Price");
-        et1.setId(numberOfAssetsInList + 1);
-        et2.setId(numberOfAssetsInList + 1);
+        et1.setHint(R.string.asset_name);
+        et2.setHint(R.string.asset_price);
+        et1.setId(numberOfFieldsInList + 1);
+        et2.setId(numberOfFieldsInList + 2);
         ll.addView(et1);
         ll.addView(et2);
-        numberOfAssetsInList++;
+        numberOfFieldsInList = numberOfFieldsInList + 2;
     }
 
-    //TODO: Add deleteAssetsInventoryItem
-
-    /*public void deleteAssetsInventoryItem() {
+    public void removeAssetsInventoryItem() {
         LinearLayout ll = (LinearLayout) findViewById(R.id.linearLayoutAssetsInventoryList);
         LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        EditText et1 = et1.getId();
-        EditText et2 = new EditText(this);
-        ll.addView(et1);
-        ll.addView(et2);
-        numberOfAssetsInList++;
-    }*/
+        EditText et1 = (EditText) findViewById(numberOfFieldsInList - 1);
+        EditText et2 = (EditText) findViewById(numberOfFieldsInList);
+        ll.removeView(et1);
+        ll.removeView(et2);
+        numberOfFieldsInList = numberOfFieldsInList - 2;
+    }
 }
