935755a222d2b004a410708066e68c630d0d63e1
Zach Goldman <zgoldman@protonmail.com>
2021-08-05 00:57:21 -0700

    Add Stock Market API
    Used AlphaVantage API to retrieve stock data, got it in a working state

--- app/src/main/java/com/example/personalfinanceapp/Frag2.java
+++ app/src/main/java/com/example/personalfinanceapp/Frag2.java
@@ -11,19 +11,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Frag2 extends Fragment {
-    Button viewInvestmentPerformance, rebalancePortfolio;
+    Button investmentPerformance, rebalancePortfolio;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frag2_layout, container, false);
-        viewInvestmentPerformance = view.findViewById(R.id.buttonViewInvestmentPerformance);
+        investmentPerformance = view.findViewById(R.id.buttonInvestmentPerformance);
         rebalancePortfolio = view.findViewById(R.id.buttonRebalancePortfolio);
 
-        viewInvestmentPerformance.setOnClickListener(new View.OnClickListener() {
+        investmentPerformance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openViewInvestmentPerformance();
+                openInvestmentPerformance();
             }
         });
 
@@ -37,7 +37,7 @@ public class Frag2 extends Fragment {
         return view;
     }
 
-    public void openViewInvestmentPerformance() {
+    public void openInvestmentPerformance() {
         Intent intent = new Intent(getActivity(), InvestmentPerformanceActivity.class);
         startActivity(intent);
     }
--- app/src/main/java/com/example/personalfinanceapp/InvestmentPerformanceActivity.java
+++ app/src/main/java/com/example/personalfinanceapp/InvestmentPerformanceActivity.java
@@ -1,6 +1,46 @@
 package com.example.personalfinanceapp;
 
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.crazzyghost.alphavantage.AlphaVantage;
+import com.crazzyghost.alphavantage.AlphaVantageException;
+import com.crazzyghost.alphavantage.Config;
+import com.crazzyghost.alphavantage.parameters.Interval;
+import com.crazzyghost.alphavantage.parameters.OutputSize;
+
+import static com.example.personalfinanceapp.BuildConfig.AlphaVantageKey;
+
 public class InvestmentPerformanceActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Config cfg = Config.builder()
+                .key(AlphaVantageKey)
+                .timeOut(10)
+                .build();
+
+        AlphaVantage.api().init(cfg);
+
+        AlphaVantage.api()
+                .timeSeries()
+                .daily()
+                .forSymbol("LGND")
+                .outputSize(OutputSize.FULL)
+                .onSuccess(e->handleSuccess(e))
+                .onFailure(e->handleFailure(e))
+                .fetch();
+    }
+
+    private void handleFailure(AlphaVantageException e) {
+        Toast.makeText(this, "Failed to retrieve stock data", Toast.LENGTH_LONG).show();
+    }
+
+    private void handleSuccess(Object e) {
+        Toast.makeText(this, "Stock data retrieved successfully", Toast.LENGTH_LONG).show();
+        System.out.println(e);
+    }
+
 }
