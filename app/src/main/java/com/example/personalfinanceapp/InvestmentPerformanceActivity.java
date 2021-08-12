d3459c67b2dc8dec86d7a84bc7d84a78ac1e6143
Zach Goldman <zgoldman@protonmail.com>
2021-08-12 02:47:13 -0700

    Add Dynamic Retrieval of Investment Information
    User can select a ticker symbol from a spinner and display Open and
    Close information in the app. Still need to dynamically populate
    spinner.

--- app/src/main/java/com/example/personalfinanceapp/InvestmentPerformanceActivity.java
+++ app/src/main/java/com/example/personalfinanceapp/InvestmentPerformanceActivity.java
@@ -1,7 +1,13 @@
 package com.example.personalfinanceapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,8 +22,31 @@ import com.crazzyghost.alphavantage.parameters.OutputSize;
 import static com.example.personalfinanceapp.BuildConfig.AlphaVantageKey;
 
 public class InvestmentPerformanceActivity extends AppCompatActivity {
+    private Spinner spinnerTickerSymbols;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.frag2_investment_performance_layout);
+        Button buttonRequestStockData = (Button) findViewById(R.id.buttonRetrieveStockData);
+        spinnerTickerSymbols = (Spinner) findViewById(R.id.spinnerTickerSymbol);
+
+        //arrayOfTickerSymbols.add("LGND");
+        //spinnerTickerSymbols.setAdapter(R.id.);
+
+        //ArrayList<String> arrayOfTickerSymbols = new ArrayList<>();
+        //arrayOfTickerSymbols.add("LGND");
+        //ArrayAdapter<String> arrayAdapter =
+
+        buttonRequestStockData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                retrieveStockData();
+            }
+        });
+
+    }
+
+    private void retrieveStockData() {
+        String tickerSymbol = this.spinnerTickerSymbols.getSelectedItem().toString();
 
         Config cfg = Config.builder()
                 .key(AlphaVantageKey)
@@ -29,7 +58,7 @@ public class InvestmentPerformanceActivity extends AppCompatActivity {
         AlphaVantage.api()
                 .timeSeries()
                 .daily()
-                .forSymbol("LGND")
+                .forSymbol(tickerSymbol)
                 .outputSize(OutputSize.FULL)
                 .onSuccess(e->handleSuccess(e))
                 .onFailure(e->handleFailure(e))
@@ -41,19 +70,29 @@ public class InvestmentPerformanceActivity extends AppCompatActivity {
     }
 
     private void handleSuccess(Object e) {
-        Pattern p1 = Pattern.compile("open=([\\d\\.]*), high=([\\d\\.]*), low=([\\d\\.])*, close=([\\d\\.])*, adjustedClose=([\\d\\.])*, volume=([\\d]*), dividendAmount=([\\d\\.]*), splitCoefficient=([\\d\\.]*), " +
+        Pattern p1 = Pattern.compile("open=([\\d\\.]*), high=([\\d\\.]*), low=([\\d\\.]*), close=([\\d\\.]*), adjustedClose=([\\d\\.]*), volume=([\\d]*), dividendAmount=([\\d\\.]*), splitCoefficient=([\\d\\.]*), " +
                         "date=([\\d]{4}-[\\d]{2}-[\\d]{2})");
         Matcher m1 = p1.matcher(e.toString());
+        TextView textViewOpen = (TextView) findViewById(R.id.textViewOpeningPrice);
+        TextView textViewClose = (TextView) findViewById(R.id.textViewClosingPrice);
+        TextView textViewDate = (TextView) findViewById(R.id.textViewDate);
 
         if (!m1.find()) {
+            //TODO: Make this a Toast
             System.out.println("Failed to retrieve stock data");
         } else {
-            System.out.println("Open: " + m1.group(1));
-            System.out.println("High: " + m1.group(2));
-            System.out.println("Low: " + m1.group(3));
-            System.out.println("Close: " + m1.group(4));
-            System.out.println("Adjusted Close: " + m1.group(5));
-            System.out.println("Date: " + m1.group(9));
+            if (m1.group(9).equals("2021-08-11")) {
+                System.out.println("Open: " + m1.group(1));
+                System.out.println("High: " + m1.group(2));
+                System.out.println("Low: " + m1.group(3));
+                System.out.println("Close: " + m1.group(4));
+                System.out.println("Adjusted Close: " + m1.group(5));
+                System.out.println("Date: " + m1.group(9));
+
+                textViewOpen.setText(m1.group(1));
+                textViewClose.setText(m1.group(4));
+                textViewDate.setText(m1.group(9));
+            }
         }
     }
 
